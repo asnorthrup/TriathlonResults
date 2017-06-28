f95d6dac77408a9a1c47233d0a202fad1c71da47
Andrew Northrup <andrew.northrup@gmail.com>
2017-06-28 12:58:43 -0400

    heroku deploy added back database.yml without prod

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,6 @@ module Triresults
     Mongoid.load!('./config/mongoid.yml')
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    #config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
