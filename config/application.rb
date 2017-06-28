33081870bcdfe435f8e9ecad8650f137eae17b27
Andrew Northrup <andrew.northrup@gmail.com>
2017-06-28 14:11:16 -0400

    heroku deploy removed active record

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
-#require "rails"
-#require "active_model/railtie"
-#require "active_job/railtie"
-#require "action_controller/railtie"
-#require "action_mailer/railtie"
-#require "action_view/railtie"
-#require "sprockets/railtie"
-#require "rails/test_unit/railtie"
+#require 'rails/all'
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 
 # Require the gems listed in Gemfile, including any gems
